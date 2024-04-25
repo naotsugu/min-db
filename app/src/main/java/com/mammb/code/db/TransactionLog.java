edc71452f53eace3aa0961486f7ba0d8fa40fed6
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-04-25 21:44:53 +0900

    Add TransactionLog

---
+++ app/src/main/java/com/mammb/code/db/TransactionLog.java
@@ -0,0 +1,92 @@
+package com.mammb.code.db;
+
+import java.util.Iterator;
+
+public class TransactionLog {
+    private DataFile dataFile;
+    private String name;
+    private Page logPage;
+    private BlockId currentBlockId;
+    private int latestLSN = 0;
+    private int lastSavedLSN = 0;
+
+    public TransactionLog(DataFile dataFile, String name) {
+        this.dataFile = dataFile;
+        this.name = name;
+        this.logPage = new Page(new byte[dataFile.blockSize()]);
+
+        int logSize = dataFile.length(name);
+        if (logSize == 0) {
+            currentBlockId = appendNewBlock();
+        } else {
+            currentBlockId = new BlockId(name, logSize - 1);
+            dataFile.read(currentBlockId, logPage);
+        }
+
+    }
+    public void flush(int lsn) {
+        if (lsn >= lastSavedLSN) {
+            flush();
+        }
+    }
+
+    public synchronized int append(byte[] logrec) {
+        int boundary = logPage.getInt(0);
+        int recSize = logrec.length;
+        int bytesNeeded = recSize + Integer.BYTES;
+        if (boundary - bytesNeeded < Integer.BYTES) {
+            // the log record doesn't fit, so move to the next block.
+            flush();
+            currentBlockId = appendNewBlock();
+            boundary = logPage.getInt(0);
+        }
+        int recPos = boundary - bytesNeeded;
+
+        logPage.setBytes(recPos, logrec);
+        logPage.setInt(0, recPos); // the new boundary
+        latestLSN += 1;
+        return latestLSN;
+    }
+
+    public Iterator<byte[]> iterator() {
+        flush();
+        return new Iterator<>() {
+            private BlockId blockId = currentBlockId;
+            private Page page = new Page(new byte[dataFile.blockSize()]);
+            private int currentPos;
+            private int boundary;
+
+            @Override
+            public boolean hasNext() {
+                return currentPos < dataFile.blockSize() || blockId.number() > 0;
+            }
+            @Override
+            public byte[] next() {
+                if (currentPos == dataFile.blockSize()) {
+                    blockId = new BlockId(name, blockId.number() - 1);
+                    dataFile.read(blockId, page);
+                    boundary = page.getInt(0);
+                    currentPos = boundary;
+                }
+                byte[] rec = page.getBytes(currentPos);
+                currentPos += Integer.BYTES + rec.length;
+                return rec;
+            }
+        };
+    }
+
+    private void flush() {
+        dataFile.write(currentBlockId, logPage);
+        lastSavedLSN = latestLSN;
+    }
+
+    private BlockId appendNewBlock() {
+        BlockId blockId = dataFile.append(name);
+        logPage.setInt(0, dataFile.blockSize());
+        dataFile.write(blockId, logPage);
+        return blockId;
+    }
+
+
+
+}
