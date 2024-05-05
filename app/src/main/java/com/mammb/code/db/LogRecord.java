f5d0b69be108041dfd9c4f8c608e4d823c8668b0
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-05-05 21:28:15 +0900

    Modify LogRecord

--- app/src/main/java/com/mammb/code/db/LogRecord.java
+++ app/src/main/java/com/mammb/code/db/LogRecord.java
@@ -6,8 +6,8 @@ public sealed interface LogRecord {
     record Start(int txn) implements LogRecord {}
     record Commit(int txn) implements LogRecord {}
     record Rollback(int txn) implements LogRecord {}
-    record SetInt(int txn) implements LogRecord {}
-    record SetString(int txn) implements LogRecord {}
+    record SetInt(int txn, int offset, int val, BlockId blockId) implements LogRecord {}
+    record SetString(int txn, int offset, String val, BlockId blockId) implements LogRecord {}
 
 
     static LogRecord createLogRecord(byte[] bytes) {
@@ -17,9 +17,29 @@ public sealed interface LogRecord {
             case 1 -> new Start(p.getInt(Integer.BYTES));
             case 2 -> new Commit(p.getInt(Integer.BYTES));
             case 3 -> new Rollback(p.getInt(Integer.BYTES));
-            case 4 -> new SetInt(p.getInt(Integer.BYTES));
-            case 5 -> new SetString(p.getInt(Integer.BYTES));
+            case 4 -> {
+                Pos pos = Pos.of(p);
+                yield new SetInt(p.getInt(pos.txn), p.getInt(pos.offset), p.getInt(pos.value),
+                    new BlockId(p.getString(pos.file), p.getInt(pos.block)));
+            }
+            case 5 -> {
+                Pos pos = Pos.of(p);
+                yield new SetString(p.getInt(pos.txn), p.getInt(pos.offset), p.getString(pos.value),
+                    new BlockId(p.getString(pos.file), p.getInt(pos.block)));
+            }
             default -> throw new RuntimeException();
         };
     }
+
+    record Pos(int txn, int file, int block, int offset, int value) {
+        static Pos of(Page page) {
+            int tpos = Integer.BYTES;
+            int fpos = tpos + Integer.BYTES;
+            int bpos = fpos + Page.maxLength(page.getString(fpos).length());
+            int opos = bpos + Integer.BYTES;
+            int vpos = opos + Integer.BYTES;
+            return new Pos(tpos, fpos, bpos, opos, vpos);
+        }
+    }
+
 }
