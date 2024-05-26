8776143b4b331331b4c2f323afd4087343d6e087
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-05-26 13:17:31 +0900

    Add Planner

---
+++ app/src/main/java/com/mammb/code/db/plan/Planner.java
@@ -0,0 +1,42 @@
+package com.mammb.code.db.plan;
+
+import com.mammb.code.db.Transaction;
+import com.mammb.code.db.query.*;
+
+public class Planner {
+    private final QueryPlanner queryPlanner;
+    private final UpdatePlanner updatePlanner;
+
+    public Planner(QueryPlanner queryPlanner, UpdatePlanner updatePlanner) {
+        this.queryPlanner = queryPlanner;
+        this.updatePlanner = updatePlanner;
+    }
+
+    public Plan createQueryPlan(String qry, Transaction tx) {
+        Parser parser = Parser.of(qry);
+        QueryData data = parser.query();
+        verifyQuery(data);
+        return queryPlanner.createPlan(data, tx);
+    }
+
+    public int executeUpdate(String cmd, Transaction tx) {
+        Parser parser = Parser.of(cmd);
+        Object data = parser.updateCmd();
+        verifyUpdate(data);
+        return switch (data) {
+            case InsertData insertData -> updatePlanner.executeInsert(insertData, tx);
+            case DeleteData deleteData -> updatePlanner.executeDelete(deleteData, tx);
+            case ModifyData modifyData -> updatePlanner.executeModify(modifyData, tx);
+            case CreateTableData createTable -> updatePlanner.executeCreateTable(createTable, tx);
+            case CreateIndexData createIndex -> updatePlanner.executeCreateIndex(createIndex, tx);
+            default -> 0;
+        };
+    }
+
+    private void verifyQuery(QueryData data) {
+    }
+
+    private void verifyUpdate(Object data) {
+    }
+
+}
